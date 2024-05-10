c525b16a717433b779d9055c2656860f531c0aba
JENIPHER <jeniperkuki@gmail.com>
2024-05-11 00:14:19 +0300

    add task button

--- todoApp/src/components/inputField.jsx
+++ todoApp/src/components/inputField.jsx
@@ -1,3 +1,5 @@
+import { FaPlus } from "react-icons/fa6";
+
 const InputField = () => {
   return (
     <div className="flex items-center">
@@ -6,8 +8,9 @@ const InputField = () => {
         name="text"
         id="addTodo"
         placeholder="Add Todo Item ..."
-        className="flex-grow p-4 border-gray-300 focus:outline-none focus:border-blue-500 border-b-2"
+        className="flex-grow p-3 border-gray-300 focus:outline-none focus:border-blue-500 border-b-2"
       />
+      <button className="ml-2 p-4 bg-blue-400 rounded hover:bg-blue-600 font-bold text-white"><FaPlus></FaPlus></button>
     </div>
   );
 };
