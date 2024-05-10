4706497d776d9d2b87be3fb98742cc72f629f727
JENIPHER <jeniperkuki@gmail.com>
2024-05-10 23:10:06 +0300

    debug

--- todoApp/redux/store.js
+++ todoApp/redux/store.js
@@ -2,7 +2,7 @@ import { createStore} from 'redux';
 import todoReducer from './reducers';
 // connect with browser redux extension
 import { composeWithDevTools } from '@redux-devtools/extension';
-const store = createStore(todoReducer, composeWithDevTools);
+const store = createStore(todoReducer, composeWithDevTools());
 
 export default store;
 
