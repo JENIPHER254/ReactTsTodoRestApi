9096682cd73465b4191fb6655d29cce8e0d20be9
JENIPHER <jeniperkuki@gmail.com>
2024-05-10 21:44:33 +0300

    todoReducer function init

---
+++ todoApp/redux/reducers.js
@@ -0,0 +1,5 @@
+// functions that specify how the application's state changes in response to actions dispatched to the Redux store
+const todoReducer =(state, action)=>{
+
+}
+export default todoReducer;
