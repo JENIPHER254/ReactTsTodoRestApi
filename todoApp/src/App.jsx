8c3c74651300892c18b00a67df95e7a780101a58
JENIPHER <jeniperkuki@gmail.com>
2024-05-10 21:08:27 +0300

    project restructure

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-2xl">CODING WITH JENIPHER</h1>
+      <h1 className="text-5xl">CODING WITH JENIPHER</h1>
     </>
   );
 }
