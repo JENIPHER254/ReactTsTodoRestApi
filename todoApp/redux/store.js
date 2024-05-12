65324caca095245ac7b6cbabfd51ab53d3088537
JENIPHER <jeniperkuki@gmail.com>
2024-05-12 20:55:34 +0300

    consuming api

--- todoApp/redux/store.js
+++ todoApp/redux/store.js
@@ -1,5 +1,12 @@
 import { createStore} from 'redux';
 import todoReducer from './reducers';
+
+/**
+ * connecting app to db via api
+ * 
+ * step1: import middle ware
+ */
+
 // connect with browser redux extension
 import { composeWithDevTools } from '@redux-devtools/extension';
 const store = createStore(todoReducer, composeWithDevTools());
