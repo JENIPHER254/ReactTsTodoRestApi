1d1923eee300622e9e1da962928baeff8cdacc15
JENIPHER <jeniperkuki@gmail.com>
2024-05-12 23:04:12 +0300

    debug

--- todoApp/redux/store.js
+++ todoApp/redux/store.js
@@ -1,15 +1,6 @@
 import { createStore } from "redux";
 import todoReducer from "./reducers";
 
-/**
- * connecting app to db via api
- *
- * step1: logger for getting easy system logs
- * step2: install axios
- */
-import logger from "react-logger";
-console.log(logger);
-// connect with browser redux extension
 import { composeWithDevTools } from "@redux-devtools/extension";
 const store = createStore(todoReducer, composeWithDevTools());
 
