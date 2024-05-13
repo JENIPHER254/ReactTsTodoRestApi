b3bdb9479fbe599d89d96c28d0f899b251e33c6d
JENIPHER <jeniperkuki@gmail.com>
2024-05-13 19:35:57 +0300

    Consuming Get API & cors installation/ setup

--- todoApi/app.js
+++ todoApi/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+
 //importing routes
 const todoItemRoute = require("./apis/todo.router");
-
+// Enable CORS for all requests
+app.use(cors());
 app.use(express.json());
 app.use('/api/',todoItemRoute);
 
